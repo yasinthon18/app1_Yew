7410f3df4a599594f1bc50711df54be1a046a11c
yasinthon18 <s6603052423031@email.kmutnb.ac.th>
2024-12-18 16:00:11 +0700

    CSS Styles with JSX

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
+import './style.css'
 
 function App() {
   return (
-    <table border="1">
-      <tr><th>Product</th><th>Price</th></tr>
-      <tr><td>React</td><td>320</td></tr>
-      <tr><td>React Native</td><td>340</td></tr>
-    </table>
+    <>
+    <h2>DeveloperThai.com</h2>
+    <div className='title'>React & React Native </div>
+    </>
   );
 }
 
