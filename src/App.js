da4387e1f384151d62d17c167426c3fcdc648fdb
yasinthon18 <s6603052423031@email.kmutnb.ac.th>
2025-01-08 15:25:52 +0700

    add event-data.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Calculator2 } from './calculator'
+import { EventData2 } from './event-data'
 
 
 
 export default function App(){
-  return <Calculator2/>
+  return <EventData2/>
 }
