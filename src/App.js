8042eb9e72ed0bf7fb9419caed2afd5a71a17d87
yasinthon18 <s6603052423031@email.kmutnb.ac.th>
2025-01-29 15:29:33 +0700

    add context-header2.js and context-content2.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { userContext } from './context'
-import Content from './context-content'
+import Content2 from './context-content2'
+import Header2 from './context-header2'
 
 
 export default function App(){
+  let [user,setUser]= React.useState()
   return (
-    <userContext.Provider value={'Yasinthon Siwarat'}>
-      <Content/>
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+      
     </userContext.Provider>
   )
 }
