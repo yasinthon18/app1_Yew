5d8cc5e849961cd407c94ac76865a35ca556d8d4
yasinthon18 <s6603052423031@email.kmutnb.ac.th>
2025-02-19 14:01:00 +0700

    add navlink.css and router.js
    add navlink.css and router.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import { userContext } from './context'
-import Content2 from './context-content2'
-import Header2 from './context-header2'
+import Router2 from './router2'
 
 
 export default function App(){
-  let [user,setUser]= React.useState()
+
   return (
-    <userContext.Provider value={[user,setUser]}>
-      <Header2/>
-      <Content2/>
-      
-    </userContext.Provider>
+    <Router2/>
   )
 }
